397085d9da0f770b78c1ae8f84ce9f05d2edca31
Moeed Furqan <aigmalik123@gmail.com>
2023-09-22 12:54:44 +0500

    Installed Databas using mongodb

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { home } = require("nodemon/lib/utils");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+mongoose.connect("mongodb://0.0.0.0:27017/blogDB");
+const postSchema = {
+  title : String,
+  content : String
+}
+
+const Post = mongoose.model("Post",postSchema);
+
+const homeStartingContent = "Welcome to our blogging haven! Discover captivating articles, expert insights, and thought-provoking stories. Unleash your creativity and share your voice with the world. Join our vibrant community of writers and readers today. Explore diverse topics, engage in discussions, and be inspired. Empower your writing journey and connect with like-minded individuals. Happy blogging!"
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let Posts = [];
+
 
 const app = express();
 
@@ -19,10 +28,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get( "/", (req,res) => {
-  res.render("home.ejs" , {
-    HomeStartContent : homeStartingContent,
-    Post : Posts
-  });
+  Post.find({})
+    .then(function(post){
+      res.render("home.ejs" , {
+        HomeStartContent : homeStartingContent,
+        Post : post
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  
 } )
 
 app.get( "/contact", (req,res) => {
@@ -41,24 +57,26 @@ app.get( "/compose", (req,res) => {
   res.render("compose.ejs" );
 } );
 
-app.get("/post/:route", (req,res) => {
-  Posts.forEach((post) => {
-    if ( _.lowerCase(post.title) === _.lowerCase(req.params.route) ){
+app.get("/post/:postid", (req,res) => {
+  const RequestedPostId = req.params.postid;
+  Post.findOne({_id : RequestedPostId})
+    .then((post)=>{
       res.render("post.ejs", {
-        Post : post,
+        Post : post
       })
-    }
-  })
-
+    })
+    .catch( err => {
+      console.log(err);
+    })
 })
 
 app.post("/compose", (req,res) => {
   // console.log(req.body.postTitle);
-  const NewContent = {
+  const NewContent = new Post({
     title : req.body.postTitle,
     content : req.body.postBody,
-  }
-  Posts.push(NewContent);
+  })
+  NewContent.save();
   res.redirect("/");
 
 });
